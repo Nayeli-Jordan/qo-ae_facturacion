6c0c5647809a4ed62af8dd6dd49d63093af801dc
Nayeli Jordan <[nayeli@queonda.com.mx]>
2018-11-07 10:36:01 -0600

    configuración wp_mail y smtp para mail facturación

--- themes/facturacion/functions.php
+++ themes/facturacion/functions.php
@@ -169,6 +169,27 @@ function auto_login_new_user( $user_id ) {
 add_action( 'user_register', 'auto_login_new_user' );
 
 
+/* Send mail by SMTP */
+add_action( 'phpmailer_init', 'send_smtp_email' );
+function send_smtp_email( $phpmailer ) {
+    $phpmailer->isSMTP();
+    $phpmailer->Host       = SMTP_HOST;
+    $phpmailer->SMTPAuth   = SMTP_AUTH;
+    $phpmailer->Port       = SMTP_PORT;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->Username   = SMTP_USERNAME;
+    $phpmailer->Password   = SMTP_PASSWORD;
+    $phpmailer->From       = SMTP_FROM;
+    $phpmailer->FromName   = SMTP_FROMNAME;
+}
+
+/* $message wp_mail in html (not text/plain) */
+function transforme_content_type(){
+    return "text/html";
+}
+add_filter( 'wp_mail_content_type','transforme_content_type' );
+
+
 /**
 * CUSTOM FUNCTIONS ALERTAS FACTURACIÓN
 */
--- themes/facturacion/page-send-alerts.php
+++ themes/facturacion/page-send-alerts.php
@@ -1,4 +1,19 @@
-<?php get_header(); ?>
+<?php get_header(); 
+
+	/* Email Header, haya o no facturaciones */
+	/* to do cambiar a correo final alto empleo contacto */
+   	/*$to 				= "nayeli@queonda.com.mx";*/
+    $subject 		 = "Alertas de facturación Alto Empleo";
+
+	$messageHeader 	 = '<html style="font-family: Arial, sans-serif;"><body>';
+	$messageHeader 	.= '<div style="text-align: center; background-color: #00B4EF; margin-bottom: 20px;"><img style="display: inline-block; margin: auto;" src="' . THEMEPATH . 'images/email/logo-light.png"></div>';
+	$messageHeader 	.= '<h1 style="display: block; margin-bottom: 30px; text-align: center;  font-size: 20px; font-weight: 700; color: #00B4EF; text-transform: uppercase;">Pr&oacute;ximas Facturaciones</h1>';
+
+	$messageFooter	 = '<br><p style="text-align: center;"><a style="color: #00B4EF; font-weight: 700; text-decoration: none; text-transform: uppercase;" href="' . SITEURL . 'ae_alertas/">Ver todas</a></p>';
+	$messageFooter  .= '<div style="text-align: center; margin-bottom: 20px;"><a style="color: #000; text-align: center; display: block;" href="' . SITEURL . '"><img style="display: inline-block; margin: auto;" src="' . THEMEPATH . 'images/email/logoae.png"></a></div>';
+	$messageFooter  .= '</body></html>';
+
+?>
 	<!-- Al cargar la página se envía un mensaje con las alertas de facturación de hoy,mañana y pasado mañana, si no hay alertas aún así se envía el email indicando que no hay facturaciones próximas -->
 	<div class="title-ae">
 		<div class="container">			
@@ -15,63 +30,66 @@
 		</div>
 	</section>
 	<?php 
-	    $args = array(
-	        'post_type' 		=> 'ae_alertas',
-	        'posts_per_page' 	=> -1,
-	        'orderby' 			=> 'title',
-	        'order' 			=> 'DESC',
-	    );
-	    $loop = new WP_Query( $args );
-	    if ( $loop->have_posts() ) {
-	    	$i = 0;
+	$terms = get_terms('tax_ejecutivo');
+	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+		foreach($terms as $term){
+			/*Obtener header por ejecutivo */
+			$to 			 = $term->description;
+			$from 			 = 'nayeli@queonda.com.mx';
+			$headers 		 = '';
+			$headers 		.= "From:" . strip_tags($from) . "\r\n";
+		    $headers   		.= "Reply-To: " . strip_tags($from) . "\r\n";
+		    $headers   		.= "MIME-Version: 1.0\r\n";
+			$headers   		.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-			/* Email Header, haya o no facturaciones */
-			/* to do cambiar a correo final alto empleo contacto */
-		   	$to 				= "nayeli@queonda.com.mx";
-		    $subject 			= "Alertas de facturación Alto Empleo";	
+			/* Obtener facturaciones de este Ejecutivo*/
+			$body = '';
 
-		    $headers 			= "From:" . strip_tags($to) . "\r\n";
-		    $headers 		   .= "Reply-To: " . strip_tags($to) . "\r\n";
-		    $headers 		   .= "MIME-Version: 1.0\r\n";
-			$headers 		   .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+		    $args = array(
+		        'post_type' 		=> 'ae_alertas',
+		        'posts_per_page' 	=> -1,
+		        'orderby' 			=> 'title',
+		        'order' 			=> 'DESC',
+				'tax_query' 		=> array(
+					array(
+						'taxonomy' 	=> 'tax_ejecutivo',
+						'field'	   	=> 'slug',
+						'terms'	 	=> $term->slug,
+					)
+				)
+		    );
+		    $loop = new WP_Query( $args );
+		    if ( $loop->have_posts() ) {
+		    	$i = 0;
+		        while ( $loop->have_posts() ) : $loop->the_post(); 
+					$post_title		= get_the_title();
+					include (TEMPLATEPATH . '/templates/ae-alertas/ae-alertas-function.php');	
 
-			$message 			 = '<html style="font-family: Arial, sans-serif;"><body>';
-			$message 			 = '<div style="text-align: center; background-color: #00B4EF; margin-bottom: 20px;"><img style="display: inline-block; margin: auto;" src="' . THEMEPATH . 'images/email/logo-light.png"></div>';
-			$message 			.= '<h1 style="display: block; margin-bottom: 30px; text-align: center;  font-size: 20px; font-weight: 700; color: #00B4EF; text-transform: uppercase;">Pr&oacute;ximas Facturaciones</h1>';
-			//echo "HEADER EMAIL <br>";
+					/*Si falta menos de dos días para la Facturación visualizar: */
+					if ($proxFacturacion >= $today && $proxFacturacion <= $FacturaAlert) : 
 
-	        while ( $loop->have_posts() ) : $loop->the_post(); 
-				$post_title		= get_the_title();
-				include (TEMPLATEPATH . '/templates/ae-alertas/ae-alertas-function.php');	
+						/* Conocer si al menos hay una facturación próxima (al día siguiente) para enviar el mail */
+						$i++;
 
-				/*Si falta menos de dos días para la Facturación visualizar: */
-				if ($proxFacturacion >= $today && $proxFacturacion <= $FacturaAlert) : 
+						$body 			.= '<p style="color: #00B4EF; text-transform: uppercase; font-size: 18px;"><strong>' . $post_title . '</strong></p>';
+						$body 			.= '<p><strong>Inicio Contrato: </strong>' . $dateContratoEsp . '</p>';
+						$body 			.= '<p><strong>Periodo de Facturaci&oacute;n: </strong>' . $periodoFacturacion  . '</p>';
+						$body 			.= '<p style="margin-bottom: 20px;"><strong>Pr&oacute;xima Facturaci&oacute;n: </strong>' . $proxFacturacionEsp  . '</p>';
+						/*$message 			.= '<hr style="height: 1px; border: 0px; background-color: #00b4ef;">';*/
 
-					/* Conocer si al menos hay una facturación próxima (al día siguiente) para enviar el mail */
-					$i++;
+					endif;
+			    endwhile; 
 
-					$message 			.= '<p style="color: #00B4EF; text-transform: uppercase; font-size: 20px;"><strong>' . $post_title . '</strong></p>';
-					$message 			.= '<p><strong>Inicio Contrato: </strong>' . $dateContratoEsp . '</p>';
-					$message 			.= '<p><strong>Periodo de Facturaci&oacute;n: </strong>' . $periodoFacturacion  . '</p>';
-					$message 			.= '<p style="margin-bottom: 20px;"><strong>Pr&oacute;xima Facturaci&oacute;n: </strong>' . $proxFacturacionEsp  . '</p>';
-					/*$message 			.= '<hr style="height: 1px; border: 0px; background-color: #00b4ef;">';*/
-					//echo $post_title . ' <br>';
+			    if ($i != 0) {
+			    	$message = $messageHeader . $body . $messageFooter;
+					echo $to . '<br>' . $message . '<br><br><br>';
+				    //mail($to, utf8_decode($subject), utf8_decode($message), $headers);
+				    wp_mail($to, $subject, $message);
+				}
+				
+		   	} 
+		    wp_reset_postdata();
 
-				endif;
-		    endwhile; 
-
-		    if ($i === 0) {
-		    	$message 			.= '<p style="text-align: center"><strong>No hay facturaciones pendientes para hoy o mañana</strong></p>';
-		    	//echo "NO HAY FACTURACIONES PARA HOY O MAÑANA";
-			}
-
-			/* Sí hubo al menos una alerta, entonces enviar email (Email Footer) */
-			$message 			.= '<br><p style="text-align: center;"><a style="color: #00B4EF; font-weight: 700; text-decoration: none; text-transform: uppercase;" href="' . SITEURL . 'ae_alertas/">Ver todas</a></p>';
-			$message	        .= '<div style="text-align: center; margin-bottom: 20px;"><a style="color: #000; text-align: center; display: block;" href="' . SITEURL . '"><img style="display: inline-block; margin: auto;" src="' . THEMEPATH . 'images/email/logoae.png"></a></div>';
-			$message	        .= '</body></html>';
-
-		    mail($to, utf8_decode($subject), utf8_decode($message), $headers);
-	   	} 
-	    wp_reset_postdata();
-	?>
+		} /*End foreach*/
+	} ?>
 <?php get_footer(); ?>
